251c3c347f9685c782cbf4df0d16d50be71a80f5
jaewooklim1 <jaewooklim1@gmail.com>
2021-04-22 14:14:21 -0400

    finished jquery followToggle and working on render

---
+++ AJAX Twitter/frontend/follow_toggle.js
@@ -0,0 +1,8 @@
+
+class FollowToggle {
+    constructor(el) {
+        this.$el = $(el);
+        this.userId = this.$el.data("user-id");
+        this.followState = this.$el.data("initial-follow-state");
+    }
+}
